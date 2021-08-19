a1115cdf542707e1924669e4a39e938e7e36441d
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-08-19 13:10:50 +0900

    [ライフサイクル]

---
+++ react-book/src/components/lifesicre.jsx
@@ -0,0 +1,19 @@
+// ライフサイクル
+// 生まれてから犬までの流れを表現したもの
+componetWillMount
+//　初期化処理で使用
+// サーバーサイドレンダリング実行時に呼ばれるメソッド
+componetWillDidMount
+// 最初のrenderが実行された直後の￥に実行されるメソッド
+// componetWillMountとは異なり、サーバーサイドレンダリング時には実行されない
+componetWillReceiveProps
+//コンポーネントがPropsの値を受け取った際に呼ばれるメソット
+shouldComponentUpdate
+
+componentWillUpdate
+
+componentDidUpDate
+
+componentWillUnmount
+
+getDerivedStateFromProps
